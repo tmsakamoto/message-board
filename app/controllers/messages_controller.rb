1704c7c8678ae56fc6926775defca973ce11d043
tmsakamoto <tmsakamoto@saintmedia.co.jp>
2016-03-03 12:27:23 +0000

    add age

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,50 +1,48 @@
 class MessagesController < ApplicationController
-  before_action :set_massege, only: [:edit, :update, :destroy]
-  
+  before_action :set_message, only: [:edit, :update, :destroy]
+
   def index
     @message = Message.new
     @messages = Message.all
   end
-  
+
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to root_path , notice: 'メッセージを保存しました'
+      redirect_to root_path, notice: 'メッセージを保存しました'
     else
-      # メッセージが保存できなかった場合
+      # メッセージが保存出来なかった場合
       @messages = Message.all
-      flash.now[:alert] = "メッセージの保存に失敗しました"
+      flash.now[:alert] = 'メッセージの保存に失敗しました'
       render 'index'
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @message.update(message_params)
       # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
+      redirect_to root_path, notice: 'メッセージを編集しました'
     else
-      # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
     end
   end
-  
+
   def destroy
     @message.destroy
-    redirect_to root_path , notice: 'メッセージを削除しました'
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
-      
   
   private
   
   def message_params
-    params.require(:message).permit(:name, :body)
+      params.require(:message).permit(:name, :age, :body)
   end
   
-  def set_massege
-    @message = Message.find(params[:id])
+  def set_message
+      @message = Message.find(params[:id])
   end
   
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
     #名前：必須、20文字以内
     validates :name , length: { maximum: 20 } , presence: true
+    # 年齢:0以上
+    validates :age , numericality: { only_intger: true, greater_than_or_equal: 0 } , allow_blank: true
     #内容：必須、2文字以上30文字以下
     validates :body ,length: { minimum: 2, maximum:30 } , presence: true
 end
